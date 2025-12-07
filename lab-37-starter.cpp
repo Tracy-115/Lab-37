5057e33f4731d0962035ce51210c506f2a8634d4
Tracy-115 <kmin984@insite.4cd.edu>
2025-12-07 15:09:35 -0800

    fixing up

--- lab-37-starter.cpp
+++ lab-37-starter.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int gen_hash_dex(const string &code){
     int tot = 0;
     for (char c : code) {
-        tot += (int)c;
+        tot += (int)c; ////adds the ascii value of the character to the total this is done for every character
     }
     return tot % 97;//i chose 97 as said in the module
 }
@@ -21,36 +21,25 @@ int gen_hash_dex(const string &code){
 int main() {
     map<int, list<string>> hash_table; //created a map named hash_table
     ifstream file("lab-37-data.txt"); //opening the file 
-    string line;
-    long long total = 0;
+    string num;
     if(!file.is_open()){
         cout << "Cannot open file" << endl;
     }
-    while (getline(file, line)) {
-        total += sum_ascii(line);
+
+    while (file >> num) {
+        int index = gen_hash_index(num); 
+        hash_table[index].push_back(num);
     }
-    while (file >> code) {
-        int index = gen_hash_index(code); 
-        hash_table[index].push_back(code);
-    }
-    char a = 'A';
-    cout << a << endl;
-    cout << (int) a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char) b << endl;
-    /*while (getline(file, line)) {
-        total += sum_ascii(line);
-    }*/
-    cout << sum_ascii("536B9DFC93AF") << endl; //this is testing
-    file.close();
-    cout << total << endl; //cout-ting the sum of all the ASCII
+    
     int count = 0;
-    for (auto &entry : hash_table)  //this prints out the index and also all the values in it
-        cout << "Index: " << entry.first << " -> ";
+    for (auto &entry : hash_table){  //this prints out the index and also all the values in it
+        cout << "Index: " << entry.first << ": ";
         for (const string &item : entry.second) {
             cout << " ";
         }
+        cout << endl; //I did three couts because I couldn't see the index well
+        cout << endl;
+        cout << endl;
         count++;
         if (count == 100) break;//this is to check the 100 enteries
     }
